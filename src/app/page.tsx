48fe605ae24fb034b0e147949712fdb3b53b48b1
Jacky <jie.zhang@grandchallenge.co.jp>
2024-03-28 11:41:39 +0900

    feat: add demo code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,75 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import AWS from "aws-sdk";
+
+import { useEffect } from "react";
+import { SignUpRequest } from "aws-sdk/clients/cognitoidentityserviceprovider";
+
+AWS.config.update({
+  region: "ap-northeast-1",
+})
+
+const ClientId = "replace with your own client id";
+
+const login = () => {
+  const payload = {
+    AuthFlow: "USER_PASSWORD_AUTH",
+    ClientId: ClientId,
+    AuthParameters : {
+        USERNAME: "newbdez33+cognitotest@abc.com",
+        PASSWORD: "1234Aa@1234567"
+    }
+  }
+  var cognito = new AWS.CognitoIdentityServiceProvider();
+  cognito.initiateAuth(payload, function(err,data) {
+      if (err) {
+        alert("Error: " + err);
+      }
+      else {
+        console.log(data);
+        alert("Success!");
+      }
+  })
+}
+
+const signUp = () => {
+  const signupRequest:SignUpRequest = {
+    ClientId: ClientId,
+    Username: "newbdez33+cognitotest@abc.com",
+    Password: "1234Aa@1234567",
+    UserAttributes: [
+      {
+        Name: "gender",
+        Value: "Male"
+      },
+      {
+        Name: "nickname",
+        Value: "nick name"
+      }
+    ]
+  }
+  var cognito = new AWS.CognitoIdentityServiceProvider();
+  cognito.signUp(signupRequest, function(err,data) {
+      if (err) {
+          alert("Error: " + err);
+      }
+      else {
+          alert("Success!");
+      }
+  })
+};
 
 export default function Home() {
+  useEffect(() => {
+    //login();
+    //signUp();
+  })
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <button onClick={login}>Login</button>
+        <button onClick={signUp}>Sign Up</button>
       </div>
     </main>
   );
